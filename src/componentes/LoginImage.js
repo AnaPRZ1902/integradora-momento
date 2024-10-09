24add327958c121e3172a5cf22d4f1f70f67bbcf
AnaPRZ1902 <anaperez51920@gmail.com>
2024-10-08 20:04:27 -0600

    cambie la imagen

--- src/componentes/LoginImage.js
+++ src/componentes/LoginImage.js
@@ -4,7 +4,7 @@ const LoginImage = () => {
   return (
     <div className="login-left">
       <h2>Inicio de sesion</h2>
-      <img src="https://img.freepik.com/premium-vector/successful-business-team-teamwork-concept_140689-2408.jpg?w=740" className='loginImagen' />
+      <img src="https://img.freepik.com/free-vector/library-background-with-books-reading-symbols-isometric-vector-illustration_1284-81743.jpg?t=st=1728439306~exp=1728442906~hmac=1e4b668781dd17ebc2c061f53ece32edf5b16732df3e3632695fea611c00b56e&w=740" className='loginImagen' />
     </div>
   );
 };
